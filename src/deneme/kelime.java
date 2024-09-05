719fc5ffbbcb67f386a2bdbcbf7c1b6e5b372391
vseprr <tunaemre.1970@gmail.com>
2024-09-05 15:05:26 +0300

    QA Automation Journey

---
+++ src/deneme/kelime.java
@@ -0,0 +1,20 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class git kelime {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen isim gir");
+        String isim= scan.nextLine();
+        System.out.println("lutfen soyisim gir");
+        String soyisim= scan.nextLine();
+        System.out.println("Lutfen yas gir");
+        double yas=scan.nextDouble();
+        System.out.println(isim);
+        System.out.println(soyisim);
+        System.out.println(yas);
+
+
+    }
+}

f024d35d0db6751e2c1c3980eebf8112598a78e5
vseprr <tunaemre1970@gmail.com>
2024-09-05 17:15:17 +0300

    My QA Automationy and Java Learning Journey C01

---
+++ src/deneme/daire.java
@@ -0,0 +1,29 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class daire {
+    public static void main(String[] args) {
+        double parsedRadius;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("------------------------------");
+            System.out.println("Lutfen bir yaricap giriniz...");
+            String input = scanner.nextLine();
+            try {
+                parsedRadius = Double.parseDouble(input);
+                double circleArea = parsedRadius * parsedRadius * Math.PI;
+                System.out.println("girmis olduğunuz dairenin alanı :" + circleArea);
+
+                if (circleArea < 100) {
+                    System.out.println("Alan 100den küçük. " + circleArea);
+                } else {
+                    System.out.println("Alana 100den büyük. " + circleArea);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Lütfen bir sayı giriniz.");
+            }
+        }
+
+    }
+}

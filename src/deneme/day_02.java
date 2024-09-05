9d133b2be2fbe746e315761d11c53a4504b80064
vseprr <tunaemre1970@gmail.com>
2024-09-05 18:05:25 +0300

    My QA Automationy and Java Learning Journey C03

---
+++ src/deneme/day_02.java
@@ -0,0 +1,13 @@
+package deneme;
+
+import static java.awt.AWTEventMulticaster.add;
+
+public class day_02 {
+    public static void main(String[] args) {
+        int number=200;
+        byte number1=(byte) number;
+        System.out.println(number1);
+
+
+    }
+}

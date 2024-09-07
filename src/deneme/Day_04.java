a211daeb03ffe0c1c7f241046b3ea9106683ea0f
vseprr <tunaemre1970@gmail.com>
2024-09-07 13:49:53 +0300

    Receiving input from the console and performing addition.

---
+++ src/deneme/Day_04.java
@@ -0,0 +1,20 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class Day_04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lütfen toplama işleminin ilk sayısını gir.");
+        int inputNumber1 = scan.nextInt();
+        System.out.println("lütfen toplama işleminin ikinci sayısını gir.");
+        int inputNumber2 = scan.nextInt();
+        System.out.println("girmiş olduğun " + inputNumber1 + " ve " + inputNumber2 + " sayılarının toplamı : " + (inputNumber1 + inputNumber2));
+        int number3 = 3;
+        int number4= 4;
+        System.out.println("" + 3+4+" java kolay");
+
+
+
+    }
+}

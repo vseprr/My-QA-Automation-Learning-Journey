dca8b4589b930330f2d90f76ceb9070e83575f5b
vseprr <tunaemre1970@gmail.com>
2024-09-08 13:54:24 +0300

    Input as a string turning into Integer and make addition procces

---
+++ src/deneme/ders_5.java
@@ -0,0 +1,17 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class ders_5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen gir");
+        String str1 = scan.next();
+        System.out.println("lutfen 2. yi gir");
+        String str2 = scan.next();
+        System.out.println(Integer.valueOf(str1)+Integer.valueOf(str2));
+
+
+
+    }
+}
